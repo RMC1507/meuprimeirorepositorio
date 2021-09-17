5d5a17cd1cb957d083f7463655411cea4bd434a4
RMC1507 <79540166+RMC1507@users.noreply.github.com>
2021-09-17 11:50:57 -0300

    Upload de Arquivos
    HTML, CSS e JavaScript.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Mensagem de Alerta 
+window.alert('JavaScript é a melhor linguagem de todas?');
+//Mensagema de Confirmação
+window.confirm('Sim');
+//Mensagem com caixa para escrita 
+window.prompt('Em qual empresa você quer trabalhar?');
+
+
